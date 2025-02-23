450b2c820e5f0a99dfcadc27f7958a51ce512874
Filow <filow1337@gmail.com>
2025-02-23 19:50:52 +0000

    updated package.json

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Index
 document.addEventListener('DOMContentLoaded', function() {
     const rotatingText = document.getElementById('rotating-text');
     const phrases = [
@@ -35,3 +36,90 @@ document.addEventListener('DOMContentLoaded', function() {
 
     type();
 });
+
+//Register
+document.getElementById('register-email-form').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const newRegisterForm = document.getElementById('register-form-container');
+    const verifyCodeForm = document.getElementById('code-form-container');
+    const registerMessageContainer = document.getElementById('register-message');
+    const codeMessageContainer = document.getElementById('register-code-message');
+
+    // Clear previous messages
+    registerMessageContainer.textContent = '';
+    codeMessageContainer.textContent = '';
+    
+    try {
+        const response = await fetch('http://localhost:5000/register', { //https://cryptotradingflow/register
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            newRegisterForm.classList.remove('active');
+            verifyCodeForm.classList.add('active');
+
+            codeMessageContainer.textContent = data.message;
+            codeMessageContainer.style.color = 'lime';
+            codeMessageContainer.style.borderColor = 'lime';
+            codeMessageContainer.style.display = 'block';
+        } else {
+            registerMessageContainer.textContent = data.message;
+            registerMessageContainer.style.color = 'red';
+            registerMessageContainer.style.borderColor = 'red';
+            registerMessageContainer.style.display = 'block';
+        }
+    } catch (error) {
+        console.error('Error during registration:', error);
+        registerMessageContainer.textContent = 'An error occurred. Please try again.';
+        registerMessageContainer.style.color = 'red';
+        registerMessageContainer.style.borderColor = 'red';
+        registerMessageContainer.style.display = 'block';
+    }
+});
+
+document.getElementById('register-code-form').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const code = document.getElementById('code').value;
+    const codeMessageContainer = document.getElementById('register-code-message');
+
+    // Clear previous messages
+    codeMessageContainer.textContent = '';
+
+    try {
+        const response = await fetch('http://localhost:5000/register', { //https://cryptotradingflow/verify-code
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ code, context: 'registration' })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            window.location.href = '/trader';
+        } else {
+            codeMessageContainer.textContent = data.message;    
+            codeMessageContainer.style.color = 'red';
+            codeMessageContainer.style.borderColor = 'red';
+            codeMessageContainer.style.display = 'block';
+        }
+    } catch (error) {
+        console.error('Error during code verification:', error);
+        codeMessageContainer.textContent = 'An error occurred. Please try again.';
+        codeMessageContainer.style.color = 'red';
+        codeMessageContainer.style.borderColor = 'red';
+        codeMessageContainer.style.display = 'block';
+    }
+});
+
